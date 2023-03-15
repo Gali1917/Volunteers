63d0c5b4610d7c6c6ad8f3cff7314a937a79876b
Gali1917 <carlgale93@hotmail.com>
2023-03-15 07:32:21 -0500

    Add/Gali1917/manifest.json, terminacion front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,12 +11,18 @@ const Index = () => {
             </div>
             <section>
                 <h1 className="welcome">BIENVENIDOS!</h1>
-                <h2>Mision</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam id delectus officiis quam vero autem. Rerum nihil dolor voluptate delectus perspiciatis, explicabo, blanditiis, repellendus aliquid excepturi quam dolorem nemo a.</p>
-                <h2>Vision</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id corrupti enim quibusdam deserunt non quo dicta eaque, maiores fugiat nobis. Impedit cumque vitae libero magni recusandae maiores porro voluptate eaque!</p>
-                <h2>Apoyanos</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos illo deleniti iste, facilis nisi sunt soluta sint rem exercitationem reprehenderit laborum delectus. Numquam modi amet, cupiditate ea ut voluptatem. Alias?</p>
+                <article>
+                    <h2>Mision</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam id delectus officiis quam vero autem. Rerum nihil dolor voluptate delectus perspiciatis, explicabo, blanditiis, repellendus aliquid excepturi quam dolorem nemo a.</p>
+                </article>
+                <article>
+                    <h2>Vision</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id corrupti enim quibusdam deserunt non quo dicta eaque, maiores fugiat nobis. Impedit cumque vitae libero magni recusandae maiores porro voluptate eaque!</p>
+                </article>
+                <article>
+                    <h2>Apoyanos</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos illo deleniti iste, facilis nisi sunt soluta sint rem exercitationem reprehenderit laborum delectus. Numquam modi amet, cupiditate ea ut voluptatem. Alias?</p>
+                </article>
             </section>
         </main>
     )
