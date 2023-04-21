2cb274731aae7149f7977c8ec26404c2c6b2e221
Gali1917 <carlgale93@hotmail.com>
2023-04-21 16:08:30 -0500

    Add/Gali1917/Pagina nosotros

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
-import { Toaster } from 'react-hot-toast';
-import Account from './pages/account';
-import Index from './pages';
-import Content from './pages/content/content';
-import Login from './pages/forms/login';
-import SignUp from './pages/forms/signUp';
-import Conditions from './pages/conditions';
-import { TareasProvider } from './context/tareasContext';
-import Details from './pages/content/details';
+import { Toaster } from "react-hot-toast";
+import Account from "./pages/account";
+import Index from "./pages";
+import Content from "./pages/content/content";
+import Login from "./pages/forms/login";
+import SignUp from "./pages/forms/signUp";
+import Conditions from "./pages/conditions";
+import Nosotros from "./pages/nosotros";
+import { TareasProvider } from "./context/tareasContext";
+import Details from "./pages/content/details";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <img src="https://i.postimg.cc/sD8GfRfB/TRIANGULO-NEGRO-VOLUNTEERS-t-1.png" alt="logo" className="logo" />
+        <img
+          src="https://i.postimg.cc/sD8GfRfB/TRIANGULO-NEGRO-VOLUNTEERS-t-1.png"
+          alt="logo"
+          className="logo"
+        />
         <nav>
           <ul>
             <Link to={"/"}>
-              <li className='primary-bottom'>
-                INICIO
-              </li>
+              <li className="primary-bottom">INICIO</li>
             </Link>
             <Link to={"/content"}>
-              <li className='primary-bottom'>
-                CONTENIDO
-              </li>
+              <li className="primary-bottom">CONTENIDO</li>
             </Link>
-            <li className='primary-bottom open-submenu'>
+            <li className="primary-bottom open-submenu">
               MI CUENTA
-              <ul className='submenu'>
+              <ul className="submenu">
                 <Link to={"/login"}>
-                  <li className='primary-bottom login'>Iniciar Sesion</li>
+                  <li className="primary-bottom login">Iniciar Sesion</li>
                 </Link>
               </ul>
             </li>
@@ -40,7 +41,6 @@ function App() {
       </header>
       <main>
         <TareasProvider>
-
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/content" element={<Content />} />
@@ -50,10 +50,17 @@ function App() {
             <Route path="/conditions" element={<Conditions />} />
             <Route path="*" element={<Index />} />
             <Route path="/details/:id" element={<Details />} />
+            <Route path="/nosotros" element={<Nosotros />} />
           </Routes>
           <Toaster />
         </TareasProvider>
       </main>
+      <footer>
+        <Link to='/nosotros'>
+          <h6>NOSOTROS</h6>
+        </Link>
+        <h6>VOLUNTEERS 2023</h6>
+      </footer>
     </div>
   );
 }
---
+++ src/pages/nosotros.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ("../styles/nosotros.css");
+
+const Nosotros = () => {
+  return (
+    <div className="div-linea">
+      <img
+        className="img-linea"
+        src="https://res.cloudinary.com/dd8a6qc45/image/upload/v1682110795/WhatsApp_Image_2023-04-12_at_22.29.41_nga34g.jpg"
+        alt="Linea de tiempo"
+      />
+    </div>
+  );
+};
+
+export default Nosotros;
